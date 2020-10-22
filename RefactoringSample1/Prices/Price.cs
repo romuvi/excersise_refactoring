ff0fdd2c966b7392103d12fe1148267e8be0f40b
Roger Muhrman <roger.muhrman@visma.com>
2020-10-22 15:39:45 +0200

    Reset repo

---
+++ RefactoringSample1/Prices/Price.cs
@@ -0,0 +1,14 @@
+﻿namespace RefactoringSample1
+{
+    abstract class Price
+    {
+        abstract public int GetPriceCode();
+
+        abstract public double GetCharge(int daysRented);
+
+        public virtual int GetFrequentPoints(int daysRented)
+        {
+            return 1;
+        }
+    }
+}
